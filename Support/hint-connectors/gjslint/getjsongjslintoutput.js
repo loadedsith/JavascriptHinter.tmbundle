a5941c97f3fdb81398ecd6d8105ded9b22fb9052
Graham Heath <heathg@google.com>
2016-07-30 19:43:19 -0700

    fix gjslint's gutter markers (file property was bad)

--- Support/hint-connectors/gjslint/getjsongjslintoutput.js
+++ Support/hint-connectors/gjslint/getjsongjslintoutput.js
@@ -1,55 +1,65 @@
 /**
  * This runner executes a linter process and collects the output from stdout.
- * The expected output from the linter is JSON that can be parsed by the renderer.
+ * The expected output from the linter is JSON that can be parsed by the
+ * renderer.
  */
 var Q = require('q'),
-  cp = require('child_process'),
-  getJsonGJSLintOutput;
+	cp = require('child_process'),
+	getJsonGJSLintOutput;
 
 
 
 /**
- * Get the json output from the linter
+ * Get the json output from the linter.
+ *
  * @param {String} runnable - Name of the executable to run
  * @param {Array} args - Command line arguments to pass
  * @param {Object} options - Options to set on process.spawn
- * @returns {Q.Promise} Returns a promise that is resolved when executable finishes running
+ * @return {Q.Promise} Returns a promise that is resolved when executable
+ *	 finishes running
  */
 getJsonGJSLintOutput = function (runnable, args, options) {
-  var def = Q.defer(),
-    dataConcat = '',
-    proc = cp.spawn(runnable, args, options || {}),
-    regex = new RegExp(/^Line (\d+), E:([^:]+): (.+)$/gm);
+	var def = Q.defer(),
+		error = '',
+		proc = cp.spawn(runnable, args, options || {}),
+		fileRegex = new RegExp(/.*?(\/.*)\ -{5}/);
+		errorRegex = new RegExp(/^Line (\d+), E:([^:]+): (.+)$/gm);
 
-  proc.stdout.on('data', function (data) {
-    dataConcat = dataConcat + data;
-  });
+	proc.stdout.on('data', function (errorDataChunk) {
+		error = error + errorDataChunk;
+	});
 
-  proc.on('close', function () {
-    var lineData = [];
-    try {
-      var  matchTest = dataConcat.match(/^.*((\r\n|\n|\r)|$)/gm);
-      for (var match of matchTest) {
-        var matchObject = {
-          file: matchTest[0],
-          hinttype: 'gjslint',
-          column: '',
-          evidence: ''
-        };
-        var matchMatches = regex.exec(match);
-        if (matchMatches) {
-          matchObject.line = matchMatches[1];
-          matchObject.message = 'E:' + matchMatches[2]+': ' + matchMatches[3];
-          lineData.push(matchObject)
-        }
-      }
-    } catch (e) {
-      lineData = [];
-    }
-    def.resolve(lineData);
-  });
+	proc.on('close', function () {
+		var errors = [];
 
-  return def.promise;
+		try {
+			var lines = error.match(/^.*((\r\n|\n|\r)|$)/gm);
+			for (var line of lines) {
+				var file = fileRegex.exec(lines[0])[1];
+				console.log('file', file);
+
+				var errorObject = {
+					file: file,
+					hinttype: 'gjslint',
+					column: null,
+					evidence: ''
+				};
+				var lineMatches = errorRegex.exec(line);
+				if (lineMatches) {
+					errorObject.line = parseInt(lineMatches[1]);
+					errorObject.message = lineMatches[3];
+					errorObject.error = 'E' + lineMatches[2];
+					errors.push(errorObject);
+				}
+			}
+		} catch (e) {
+			errors = [];
+		}
+
+		def.resolve(errors);
+	});
+
+	return def.promise;
 };
 
 module.exports = getJsonGJSLintOutput;
