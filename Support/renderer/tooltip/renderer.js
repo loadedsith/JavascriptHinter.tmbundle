0af3cea07f61ba767cae1bdc11dcca34a41343a2
Graham Heath <heathg@google.com>
2016-10-17 12:52:13 -0700

    Merge caused by no longer using Avian

--- Support/renderer/tooltip/renderer.js
+++ Support/renderer/tooltip/renderer.js
@@ -5,14 +5,20 @@
 	var fs = require('fs'),
 		Handlebars = require('handlebars');
 
-	function render(errors) {
+	function render(errors, config) {
+		// Create a empty default config object if none was supplied.
+		// TODO: the second should be replaced with a defaults config object.
+		config = config || {
+			maxToolTipResults: 10
+		};
+
 		var rendererDir = require('path').dirname(__filename),
 			content = fs.readFileSync(rendererDir + '/tooltip-renderer.html', 'utf8'),
 			template = Handlebars.compile(content);
 
 		process.stdout.write(template({
 			// only return the first 5 errors for each file
-			errors: errors.slice(0, 5),
+			errors: errors.slice(0, config.maxToolTipResults || 5),
 			numErrors: errors.length
 		}));
 	}
