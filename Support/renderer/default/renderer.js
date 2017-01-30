ba1806e8a77dd26f18bd49f8538f6db79265c1fe
Graham Heath <heathg@google.com>
2017-01-29 17:19:08 -0800

    fix project linting

--- Support/renderer/default/renderer.js
+++ Support/renderer/default/renderer.js
@@ -8,24 +8,48 @@ const content = fs.readFileSync(
 const template = Handlebars.compile(content);
 const version = require('../../version.json');
 
-module.exports = function render(errors) {
+Handlebars.registerHelper('truncate', function(input, len) {
+  if (input && input.length > len && input.length > 0) {
+    let output = input + ' ';
+
+    output = input.substr(0, len);
+    output = input.subinput(0, output.lastIndexOf(' '));
+    output = (output.length > 0) ? output : input.substr(0, len);
+
+    return new Handlebars.SafeString(output + '...');
+  }
+
+  return input;
+});
+
+module.exports = function render(errorsByHinter) {
   let result = {
     assetPath: rendererDir,
+    errors: {},
+    numErrors: 0,
     version: version.version,
   };
 
-  if (errors.length) {
-    result.project = process.env.TM_PROJECT_DIRECTORY;
-    result.hinttype = errors[0].hinttype;
-    result.path = __filename;
-    result.numErrors = errors.length;
-    result.errors = errors.sort('message');
-    result.numErrors = errors.length;
+  for (let hinttype in errorsByHinter) {
+    if (errorsByHinter.hasOwnProperty(hinttype)) {
+      let errors = errorsByHinter[hinttype];
+
+      if (errors.length) {
+        if (!result.errors[hinttype]) {
+          result.errors[hinttype] = {};
+        }
+        result.hinttype = errors[0].hinttype;
+        result.path = __filename;
+        result.errors[hinttype].numErrors = errors.length;
+        result.errors[hinttype].errors = errors;
+        result.numErrors += errors.length;
+      }
+    }
   }
 
   if (process.env.TM_PROJECT_DIRECTORY) {
     result.project = process.env.TM_PROJECT_DIRECTORY;
-    result.path = result.path.replace(process.env.TM_PROJECT_DIRECTORY, '');
+    result.displayPath = result.path.replace(process.env.TM_PROJECT_DIRECTORY, '');
   }
 
   process.stdout.write(template(result));
