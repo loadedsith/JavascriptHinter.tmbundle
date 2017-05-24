58530cf5ec3cbcf46f24cf0901377d6117c81626
Graham Heath <heathg@google.com>
2017-05-24 13:07:00 -0700

    add js_hinter defaults

---
+++ Support/.tm_jshinter_defaults.js
@@ -0,0 +1,29 @@
+/**
+ * JavascriptHinter plugin settings.
+ */
+module.exports = {
+  disabledPlugins: ['gjslint', 'jscs', 'jshint', 'eslint', 'eslint_d'],
+  ignored: [
+    '.git/**',
+    '.gitignore',
+    'node_modules/**',
+    'dist/**',
+    '.DS_store',
+    'build/**',
+    'Support/node_modules/**',
+    '.jshintrc',
+    '.jshintignore',
+    '.*.min.js',
+    '.*.min.css'
+  ],
+  eslint: {
+    args: [
+      '--config', (process.env.TM_PROJECT_DIRECTORY || process.env.HOME) + '/../.eslintrc'
+    ]
+  },
+  gjslint: {
+    args: [
+      '--disable', '0005,0001'
+    ]
+  }
+};
